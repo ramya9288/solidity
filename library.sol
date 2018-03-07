2529ef66f971a56164466257fd9c72295e1175a3
ramya9288 <37015413+ramya9288@users.noreply.github.com>
2018-03-07 15:23:56 +0530

    Create library.sol

---
+++ library.sol
@@ -0,0 +1,44 @@
+pragma solidity^0.4.0;
+contract book{
+    struct libbk{
+        string bname; 
+        uint256 bid;
+        uint256 stock;
+        uint256 count;
+        
+        }
+    struct pubbk{
+        string name;
+        uint256 id;
+    }
+    mapping (uint256=>libbk)lbk;
+    mapping (uint256=>pubbk)pbk;
+    
+     function lib(uint256 bid,string bname,uint256 stock)public {
+         lbk[bid].bid=bid;
+         lbk[bid].bname=bname;
+         lbk[bid].stock=stock;
+          lbk[bid].count++;
+          lbk[bid].stock=lbk[bid].stock-1;
+    }
+    function stu(uint256 id,string name)public {
+         pbk[id].id=id;
+         pbk[id].name=name;
+    }
+     function display(uint256 bid,uint256 id)public constant returns(uint256,string,uint256){
+               
+                   return(lbk[bid].count, lbk[bid].bname,lbk[bid].bid);
+     }
+             
+     function balance(uint256 bid)public constant  returns(uint256){
+        
+        require(lbk[bid].stock>0);
+        lbk[bid].stock=lbk[bid].stock-lbk[bid].count;
+        return(lbk[bid].stock);
+    }
+    
+   function bookre(uint256 bid)public constant returns(uint256){
+    return  (lbk[bid].stock=lbk[bid].stock+1);
+    }
+                   
+    }
