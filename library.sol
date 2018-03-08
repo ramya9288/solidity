dc83ee660bad170feffd21d3215851d9cff73015
ramya9288 <37015413+ramya9288@users.noreply.github.com>
2018-03-08 10:28:10 +0530

    Update library.sol

--- library.sol
+++ library.sol
@@ -15,36 +15,28 @@ contract book{
     mapping (uint256=>pubbk)pbk;
     
      function lib(uint256 bid,string bname,uint256 stock)public {
+         
          lbk[bid].bid=bid;
          lbk[bid].bname=bname;
          lbk[bid].stock=stock;
           lbk[bid].count++;
-        // lbk[bid].stock=lbk[bid].stock-1;
+         require(lbk[bid].stock>0);
+         lbk[bid].stock=lbk[bid].stock-lbk[bid].count;
     }
     function stu(uint256 id,string name)public {
          pbk[id].id=id;
          pbk[id].name=name;
     }
-     function display(uint256 bid,uint256 id)public constant returns(uint256,string,uint256){
-               
-                   return(lbk[bid].count, lbk[bid].bname,lbk[bid].bid);
+     function display(uint256 bid,uint256 id)public constant returns(uint256,string,uint256,uint256){
+                   return(lbk[bid].count, lbk[bid].bname,lbk[bid].bid,lbk[bid].stock);
      }
              
 
    function bookre(uint256 bid,uint256 id)public  {
      require(lbk[bid].count!=0);
-      // lbk[bid].stock=lbk[bid].stock+lbk[bid].count;
-        lbk[bid].count--;
-         lbk[bid].stock=lbk[bid].stock+lbk[bid].count;
+           lbk[bid].count--;
+         lbk[bid].stock=lbk[bid].stock+1;
         
-          // return(lbk[bid].stock);
     }
-   function balance(uint256 bid,uint256 id)public constant  returns(uint256){
-        
-        require(lbk[bid].stock>0);
-       lbk[bid].stock=lbk[bid].stock-lbk[bid].count;
-        return(lbk[bid].stock);
-    }
-                   
-    }
+ }
     
